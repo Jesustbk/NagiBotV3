4d8bbc94c59adb420e675be3d32a6e4cd178879f
𝗙𝗔𝗡𝗧𝗢𝗠 𝗨𝗪𝗨 <brayanphonkponk@gmail.com>
2025-06-28 11:01:21 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -90,7 +90,6 @@ global.cn ='https://whatsapp.com/channel/0029Vb4cQJu2f3EB7BS7o11M';
 
 global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 global.ch = {
-ch1: '120363312092804854@newsletter', //NagiBot-MD
 }
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
